c12a68f10e6b907e0deda996fe92a8b5a4efaa4e
yang-chris <18813068824@163.com>
2019-08-01 23:30:14 +0800

    双向数据绑定和states传值

--- src/App.js
+++ src/App.js
@@ -2,16 +2,53 @@ import React, { Component } from 'react';
 import Person from './components/Person/Person'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World, React</h1>
-        <Person name="杨小伟"/>
-        <Person name="Chris Yang">初来乍到,请多多观照!</Person>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: [
+      {name: '米斯特杨', age: 30},
+      {name: 'Chris yang', age: 40},
+      {name: 'Chris li', age: 50},
+    ]
+  }
+  // switchName() {
+  //   console.log(this.state.person)
+  // }
+  switchName = (name) => {
+    console.log(this.state.person)
+    this.setState({
+      person: [
+        {name: name, age: 30},
+        {name: 'Chris yang', age: 40},
+        {name: 'Chris li', age: 50},
+      ]
+    })
+  }
+  changeName = (e) => {
+    this.setState({
+      person: [
+        {name: 'name', age: 30},
+        {name: e.target.value, age: 40},
+        {name: 'Chris li', age: 50},
+      ]
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello World, React</h1>
+          {/*  箭头函数传值 
+          <button onClick={ () => this.switchName('米修')}>更改状态值</button> */}
+          <button onClick={ this.switchName.bind(this, 'bind传值米修')}>更改状态值</button>
+          <Person changed={this.changeName} name={this.state.person[0].name}/>
+          <Person
+            myclick={this.switchName.bind(this, 'props传值米修')}
+           name={this.state.person[1].name}/>
+          <Person name={this.state.person[2].name}>初来乍到,请多多观照!</Person>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -2,7 +2,8 @@ import React from 'react';
 const person = (props) => {
   return (
     <div>
-      <p>大家好，我是{props.name}，初学React！我已经{Math.floor(Math.random() * 30)}岁</p>
+      <input type="text" onChange={props.changed} />
+      <p onClick={props.myclick}>大家好，我是{props.name}，初学React！我已经{Math.floor(Math.random() * 30)}岁</p>
       <p>{props.children}</p>
     </div>
   )
