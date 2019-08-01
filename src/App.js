39bb2b1494fb61e005acd7f77c5d35ffcef8eaff
yang-chris <18813068824@163.com>
2019-08-02 00:13:53 +0800

    数据双向绑定和样式

--- src/App.js
+++ src/App.js
@@ -2,49 +2,90 @@ import React, { Component } from 'react';
 import Person from './components/Person/Person'
 import './App.css';
 
-class App extends Component {
+class App extends Component
+{
   state = {
     person: [
-      {name: '米斯特杨', age: 30},
-      {name: 'Chris yang', age: 40},
-      {name: 'Chris li', age: 50},
-    ]
+      { name: '米斯特杨', age: 30 },
+      { name: 'Chris yang', age: 40 },
+      { name: 'Chris li', age: 50 },
+    ],
+    showPerson: false
   }
   // switchName() {
   //   console.log(this.state.person)
   // }
-  switchName = (name) => {
+  switchName = (name) =>
+  {
     console.log(this.state.person)
     this.setState({
       person: [
-        {name: name, age: 30},
-        {name: 'Chris yang', age: 40},
-        {name: 'Chris li', age: 50},
+        { name: name, age: 30 },
+        { name: 'Chris yang', age: 40 },
+        { name: 'Chris li', age: 50 },
       ]
     })
   }
-  changeName = (e) => {
-    this.setState({
+  changeName = (e, index) =>
+  {
+    console.log(index,'**')
+    let person = this.state.person;
+    person[index].name = e.target.value;
+    this.setState({person})
+    /* this.setState({
       person: [
-        {name: 'name', age: 30},
-        {name: e.target.value, age: 40},
-        {name: 'Chris li', age: 50},
+        { name: 'name', age: 30 },
+        { name: e.target.value, age: 40 },
+        { name: 'Chris li', age: 50 },
       ]
-    })
+    }) */
   }
-  render(){
+  togglePerson = () =>
+  {
+    const showPerson = this.state.showPerson;
+    this.setState({ showPerson: !showPerson })
+  }
+  deletePerson = (index) => {
+    const person = this.state.person;
+    person.splice(index, 1)
+    this.setState({person})
+  }
+  render()
+  {
+    const style = {
+      textShadow: '0 5px 15px #f00'
+    }
+    let persons = null;
+    if (this.state.showPerson) {
+      persons = (
+        <div>
+          {
+            this.state.person.map((item, index) => {
+              return <Person 
+              changed={(e) => this.changeName(e, index)}
+              myclick={() => this.deletePerson(index)}
+              key={index} 
+              name={item.name} />
+            })
+          }
+          {/* <Person changed={this.changeName} name={this.state.person[0].name} />
+          <Person
+            myclick={this.switchName.bind(this, 'props传值米修')}
+            name={this.state.person[1].name} />
+          <Person name={this.state.person[2].name}>初来乍到,请多多观照!</Person> */}
+        </div>
+      )
+    }
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Hello World, React</h1>
+          <h1 style={style}>Hello World, React</h1>
           {/*  箭头函数传值 
           <button onClick={ () => this.switchName('米修')}>更改状态值</button> */}
-          <button onClick={ this.switchName.bind(this, 'bind传值米修')}>更改状态值</button>
-          <Person changed={this.changeName} name={this.state.person[0].name}/>
-          <Person
-            myclick={this.switchName.bind(this, 'props传值米修')}
-           name={this.state.person[1].name}/>
-          <Person name={this.state.person[2].name}>初来乍到,请多多观照!</Person>
+          <button onClick={this.switchName.bind(this, 'bind传值米修')}>更改状态值</button>
+          <button onClick={this.togglePerson}>内容其切换</button>
+          {persons}
+
         </header>
       </div>
     );
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Person.css'
 const person = (props) => {
   return (
     <div>
       <input type="text" onChange={props.changed} />
-      <p onClick={props.myclick}>大家好，我是{props.name}，初学React！我已经{Math.floor(Math.random() * 30)}岁</p>
+      <p className="person" onClick={props.myclick}>大家好，我是{props.name}，初学React！我已经{Math.floor(Math.random() * 100)}岁</p>
       <p>{props.children}</p>
     </div>
   )
